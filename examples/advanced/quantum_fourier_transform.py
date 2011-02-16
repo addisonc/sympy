1a24e3e13aee3181fc64557a3ab74dc0e1b41fb8
Addison CUgini <ajcugini@gmail.com>
2011-02-15 21:52:57 -0800

    Made additional changes to the examples for conference. SHould be good to go

--- examples/advanced/quantum_fourier_transform.py
+++ examples/advanced/quantum_fourier_transform.py
@@ -1,11 +1,9 @@
 
 """
 This is an example file that will demonstrate how to use the Sympy's QFT
-To do circuit ploting, matplotlib [and other libraries] needs to be installed.
-Run ipython with the --pylab flag
 """
 
-from sympy.physics.quantum.qft import QFT, IQFT
+from sympy.physics.quantum.qft import QFT, IQFT, Fourier
 from sympy.physics.quantum.qubit import Qubit
 from sympy.physics.quantum.represent import represent
 from sympy.physics.quantum.applyops import apply_operators
@@ -13,22 +11,19 @@ from sympy.physics.quantum.circuitplot import circuit_plot
 from sympy.interactive import init_printing
 from sympy import sqrt, pprint
 
-#First, initialize pretty printing
-init_printing()
+"""
+   Sympy has the ability to apply discrete fourier transforms to states
+   Function useful for period finding subroutines
+   Can decompose both Quantum Fourier and it's inverse into elementary gates
+   Can represent Discrete Fourier transform matrix
+"""
 
-#We can create a QFT operator and decompose it in terms of elementary gates
-Fourier = QFT(0,3).decompose()
-pprint(Fourier)
 
-#We can also plot the Fourier gate
-circuit_plot(Fourier, nqubits=3)
+fourier = QFT(0,3).decompose(); fourier
 
-#We can represent the QFT in a three qubit space
-pprint(represent(QFT(0,3), nqubits=3))
+close('all'); circuit_plot(fourier, nqubits=3)
 
-#We can apply the QFT to a state to put state into the frequency basis
-#The following state is something akin to what we would find in an order-finding
-#subroutine
-state = (Qubit('000') + Qubit('010') + Qubit('100') + Qubit('110'))/sqrt(4)
-pprint(apply_operators(Fourier*state))
+represent(Fourier(0,3), nqubits=3)*4/sqrt(2)
 
+state = (Qubit('000') + Qubit('010') + Qubit('100') + Qubit('110'))/sqrt(4); state
+apply_operators(fourier*state)
--- examples/advanced/quantum_teleportation.py
+++ examples/advanced/quantum_teleportation.py
@@ -10,38 +10,36 @@ from sympy import symbols
 from sympy.interactive import init_printing
 from sympy import sqrt, pprint
 
-init_printing()
+"""
+   Run this branch on Addison's normalize_fix branch: git co addison/normalize_fix
 
-#make a state that is a superposition of 0 and 1 for 3 qubits
-#This is an arbitrary symbolic superposition
-a,b = symbols('ab')
-state = Qubit('000')*a + Qubit('001')*b
-pprint(state)
+*Alice and Bob meet and entangle qubits; Alice takes Qubit 1, Bob takes 2
+*Some time later, Alice takes an arbitrary state (Qubit 0) and applies a similair entangling operator
+*Alice then measures Qubits 1 and 0
+*Alice tells Bob to apply an XGate and/or a ZGate based on measurement
+*Bob now has the arbitrary state 
+"""
+
+a,b = symbols('ab', real=True)
+state = Qubit('000')*a + Qubit('001')*b; state
 
 #This operator will entangle bits 1 and 2
-Entangle1_2 = CNOT(1,2)*HadamardGate(1)
-state = apply_operators(Entangle1_2*state)
-pprint(state)
+entangle1_2 = CNOT(1,2)*HadamardGate(1); entangle1_2
+state = apply_operators(entangle1_2*state); state
 
-#Bit one goes to Alice and bit 2 goes to Bob
+#Bit 1 goes to Alice and bit 2 goes to Bob
 
 #Now, some time later, Alics entangles Qubit 1 and Qubit 0 
-Entangle0_1 = HadamardGate(0)*CNOT(0,1)
-circuit_plot(Entangle0_1*Entangle1_2, nqubits=3)
-state = apply_operators(Entangle0_1*state)
-pprint(state)
+entangle0_1 = HadamardGate(0)*CNOT(0,1); entangle0_1
+circuit_plot(entangle0_1*entangle1_2, nqubits=3)
+state = apply_operators(entangle0_1*state); state
 
 #Alice now measures both of her qubits
-measured_result = measure_partial(state, (0,1))
-#These are possible results with given probabilities
-pprint(measured_result)
+measured_result = measure_partial(state, (0,1), normalize=False); measured_result
 
-#lets say she measured 1,0 for her bits
-state = measured_result[2][0]
-pprint(state)
+#lets say she measured 10
+state = (measured_result[2][0]*2).expand(); state
 
-#she tells bob to apply a Not Operator to his bit
-state = apply_operators(XGate(2)*state)
-pprint(state)
-
+#she tells bob to apply a Not Operator to his bit, so that bob has the original state
+state = apply_operators(XGate(2)*state); state
 
